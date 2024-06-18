9a6260aa7de52fb90c01e21abd7e321c6a9db597
trueluck2024 <trueluck007@gmail.com>
2024-06-18 23:25:40 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
       <div className="particles-background">
         <AnimatedParticles />
       </div>
-       <Router basename="/trueluck-v1">
+       <Router basename="/true-luck">
         <Routes>
           <Route path="/" element={<LandingPage/>} />
           <Route path="/Home" element={<Home disclaimerPopup={disclaimerPopup} hideDisclaimer={hideDisclaimer} />} />
